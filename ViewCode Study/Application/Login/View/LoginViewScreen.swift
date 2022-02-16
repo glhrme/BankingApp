167b7b1989b4cfeed7613ace67b5f069ecc7ae2f
Guilherme dos Santos <guilherme@guisantos.com.br>
2022-02-16 00:14:43 -0300

    Create a Username Textfield
    Not finished

--- ViewCode Study/Application/Login/View/LoginViewScreen.swift
+++ ViewCode Study/Application/Login/View/LoginViewScreen.swift
@@ -15,13 +15,24 @@ class LoginViewScreen: BaseUIView {
         return image
     }()
     
-    lazy var labelLogin: UILabel = {
+    lazy var loginLabel: UILabel = {
         var label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Seu nome de usuário"
         label.textColor = UIColor(named: .baseBlack)
         label.font = UIFont(named: .SFPro, size: 18)
         return label
+    }()
+    
+    lazy var usernameTextField: UITextField = {
+        var tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.clipsToBounds = true
+        tf.layer.cornerRadius = 4
+        tf.placeholder = "joaozinho"
+        tf.backgroundColor = UIColor(named: .baseSecondGray)
+        tf.textColor = UIColor(named: .baseBlack)
+        return tf
     }()
     
     init() {
@@ -33,13 +44,26 @@ class LoginViewScreen: BaseUIView {
 
 extension LoginViewScreen: CodeView {
     func buildViewHierrachy() {
-        self.addSubview(labelLogin)
+        self.addSubview(logoBank)
+        self.addSubview(loginLabel)
+        self.addSubview(usernameTextField)
     }
     
     func setupConstraints() {
         NSLayoutConstraint.activate([
-            self.labelLogin.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            self.labelLogin.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            self.logoBank.topAnchor.constraint(equalTo: self.topAnchor, constant: 32),
+            self.logoBank.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            self.logoBank.heightAnchor.constraint(equalToConstant: 100),
+            self.logoBank.widthAnchor.constraint(equalToConstant: 100),
+            
+            self.loginLabel.topAnchor.constraint(equalTo: self.logoBank.bottomAnchor, constant: 60),
+            self.loginLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            self.loginLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            
+            self.usernameTextField.topAnchor.constraint(equalTo: self.loginLabel.bottomAnchor, constant: 8),
+            self.usernameTextField.leadingAnchor.constraint(equalTo: self.loginLabel.leadingAnchor, constant: 0),
+            self.usernameTextField.trailingAnchor.constraint(equalTo: self.loginLabel.trailingAnchor, constant: 0),
+            self.usernameTextField.heightAnchor.constraint(equalToConstant: 60)
         ])
     }
     
